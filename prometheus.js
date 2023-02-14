6d85f4507935fa1119aaefed9790a2adeb26164a
CowboyJoe <s.rosenow@t-online.de>
2023-02-14 10:41:56 +0100

    initial commit

---
+++ prometheus.js
@@ -0,0 +1,188 @@
+'use strict'
+
+const { inspect } = require('node:util');
+
+// NOTE: Prometheus does not show the real thing, instead it captures
+// only userdefined calls, but it helps to understand a little bit better.
+// It would be great to get the real underlaying structures that really hold
+// the data. The problem is to identify each promise since they all look the
+// same from the inspector.
+
+// TODO: can be capsulated for more safety
+
+const util = require('node:util');
+// const { getPromiseDetails, PromiseStatus } = require('react-promise-utils/getPromiseDetails');
+
+class Prometheus {
+
+	constructor() {
+		this.DebugLevel = 0;
+		this.Promises = new Map();// new List([]);
+		this.Cyclic = undefined;
+		this.DeleteWhenFinished = false;
+
+		// WARNING: not sure if this is officially allowed:
+		Promise.prototype.ID = undefined;
+	}
+
+	startCyclic(delay = 1000) {
+
+		// delay ||= 1000;
+
+		if (this.DebugLevel > 0) console.log('Starting cycylic task with a delay of ' + delay + ' ms.');
+		// this.Delay = delay;
+		this.Cyclic = setInterval(() => {
+			prometheus.showRegisteredPromisesSync();
+		}, delay);
+	}
+
+	stopCyclic() {
+
+		if (this.DebugLevel > 0) console.log('Stopping cycylic task.');
+		if (this.Cyclic) {
+			clearInterval(this.Cyclic);
+		}
+	}
+
+	getPromiseStateAsync(promise) {
+
+		const t = {};
+
+		return Promise.race([promise, t])
+			.then((result) => {
+				return (result === t)? "pending" : "resolved"
+			}, (error) => {
+				return "rejected"
+			});
+	}
+
+	// TODO:
+	info2() {
+		let info = process.binding('util').getPromiseDetails(Promise.resolve({data: [1,2,3]}));
+		console.log(info);
+
+		// process.binding('util').getPromiseDetails
+
+	}
+
+	getPromiseStateSync(promise) {
+
+		if (!promise) throw('ERROR: no promise');
+
+		const s = inspect(promise, { sorted:true });
+		// console.log(s);
+
+		// NOTE: results in either of:
+		//
+		// 'Promise { 'result' }'
+		// 'Promise { <rejected> 'cause' }'
+		// 'Promise { <pending> }'
+		//
+		// where result and cause are any objects.
+
+		const regexpString = String.raw`^ Promise \s* \{ \s* (?<state><[^>]+>)? (\s+ (?<obj>.*))? (,\s*(?<data>.*))? \s* \} $`;
+		// console.log(regexpString);
+
+		const regexpString2 = regexpString.split(' ').join('');
+		// console.log(regexpString2);
+
+		const regex = new RegExp( regexpString2, 'g' );
+		// console.log(regex);
+
+		const iter = s.matchAll(regex);
+		const match = Array.from(iter);
+
+		if (match && (match.length > 0)) {
+			const state = match[0].groups['state'];
+			// console.dir( state + ' ' + match[0].groups['obj'] );
+			switch (state) {
+				case '<pending>':
+					return 'pending';
+
+				case '<rejected>':
+					return 'rejected';
+
+				default:
+					return 'resolved';
+			}
+		}
+
+		throw('Error: Could not analyze Promise! ' + s);
+	}
+
+	async showPromises2() {
+
+		// TODO:
+		let s = 'Promises [' + this.Promises.size + ']\n';
+
+		for (const [key, promise] of this.Promises) {
+
+			let r;
+			await this.getPromiseState(promise).then((data) => {
+				r = data;
+			})
+
+			this.getPromiseStateSync(promise);
+
+			s += '    ID: ' + promise.ID + ' Source: ' + promise.Source + ' State: ' + r + '\n'
+		}
+		console.log(s);
+	}
+
+	showRegisteredPromisesSync() {
+
+		let s = this.Promises.size + ' Promise(s) registered:\n';
+
+		for (const [key, promise] of this.Promises) {
+
+			const state = this.getPromiseStateSync(promise);
+			s += '    ID: ' + promise.ID + ' Source: ' + promise.Source + ' State: ' + state + '\n'
+		}
+		console.log(s);
+	}
+
+	isRegistered(promise) {
+		return( this.Promises.has(promise.ID) );
+	}
+
+	register(promise, id) {
+
+		if (promise.ID || promise.Source) throw('ERROR: promise not clean!');
+
+		promise.ID = id;
+
+		if (this.isRegistered(promise)) throw('ERROR: Promise already exists!');
+
+		if (this.DebugLevel > 0) console.log('Adding a watch to a promise with ID ' + promise.ID + ' from ' + promise.Source);
+		this.Promises.set(promise.ID, promise);
+
+		return promise;
+	}
+
+	unregister(promise) {
+
+		if (!this.isRegistered(promise.ID)) throw('ERROR: Promise not found!');
+
+		if (this.DebugLevel > 0) this.log('Removing promise with ID ' + promise.id);
+		// this.Promises = this.Promises.filter(item => item[2] !== promise);
+		delete this.Promises(promise);
+	}
+
+	process( promise, callback ) {
+
+		if (this.DebugLevel > 0) console.log('Procerssing promise with ID ' + promise.ID);
+
+		if (this.DeleteWhenFinished) this.remove(promise);
+
+		return ( () => {
+			if (this.DebugLevel > 0) console.log('callback');
+			callback();
+		}).bind(promise);
+	}
+}
+
+const prometheus = new Prometheus();
+
+module.exports = {
+	prometheus
+};
---
+++ test.js
@@ -0,0 +1,109 @@
+"use strict"
+
+// TODO: lib
+
+const { prometheus } = require('./prometheus');
+
+function testSync() {
+
+	const a = Promise.resolve('result');
+	const b = Promise.reject('cause');
+	const c = new Promise(() => {});
+
+	console.log('sync a: ' + prometheus.getPromiseStateSync(a));
+
+	// NOTE: This is working, but the rejected promise throws an exception:
+	// console.log('sync b: ' + prometheus.getPromiseStateSync(b));
+
+	// To capture the exception you could have the idea to add a .catch().
+	// this is NOT working. catch goes async and the result is pending.
+	// For the WHOLE expression it IS correct but it does not show the
+	// rejected promise.
+	// console.log('sync b: ' + prometheus.getPromiseStateSync(b.catch((error) => {})));
+
+	// Instead we register an event handler to not crash the app.
+	process.on('unhandledRejection', function(err, promise) {
+		console.log('Unhandled rejection (promise: ', promise, ', reason: ', err, ').');
+	});
+
+	// now this is working:
+	console.log('sync b: ' + prometheus.getPromiseStateSync(b));
+
+	console.log('sync c: ' + prometheus.getPromiseStateSync(c))
+}
+
+function testAsync() {
+
+	const a = Promise.resolve();
+	const b = Promise.reject();
+	const c = new Promise(() => {});
+
+	prometheus.getPromiseStateAsync(a).then(state => console.log('async a: ' + state)); // fulfilled
+	prometheus.getPromiseStateAsync(b).then(state => console.log('async b: ' + state)); // rejected
+	prometheus.getPromiseStateAsync(c).then(state => console.log('async c: ' + state)); // pending
+}
+
+function testSyncWithCyclicUpdate() {
+
+	// external lib
+	// TODO:
+
+	const a = Promise.resolve();
+	const b = Promise.reject();
+	const c = new Promise(() => {});
+
+	const intervalId = setInterval(() => {
+		const [itemsPromiseStatus, items] = getPromiseDetails(a); // lib
+
+		console.log(itemsPromiseStatus);
+
+		if (itemsPromiseStatus === PromiseStatus.REJECTED) {
+			clearInterval(intervalId);
+		}
+
+		if (itemsPromiseStatus === PromiseStatus.FULFILLED) {
+			clearInterval(intervalId);
+			console.log(items);
+		}
+	}, 200);
+
+}
+
+function main() {
+
+	prometheus.DebugLevel = 5;
+
+	console.log('Async is started first');
+	testAsync();
+
+	console.log('Sync is started second');
+	testSync();
+
+	console.log('But Sync is _executed_ before async! This shows that sync is REALLY sync.');
+	console.log('-----------------------------');
+
+	// start cyclic
+	prometheus.startCyclic(500);
+
+	// create a promise which resolves after 3s
+	const p1 = new Promise((resolve, reject) => {
+		setTimeout(() => {
+			console.log('TICK!');
+			prometheus.process(p1, () => { // add this line before the resolve
+				resolve(5)
+			})(); // add this line after the resolve
+		}, 3000);
+	})
+
+	// register it
+	prometheus.register(p1, 'Bob');
+
+	// stop cyclic after 5s
+	setTimeout(() => {
+		prometheus.stopCyclic();
+	}, 5000);
+
+//	prometheus.testSyncWithCyclicUpdate();
+}
+
+main();
